639169b4819a07f6777c6d23e2bb0912f3c794d6
emin <magomedemin.islamov@mail.ru>
2020-12-16 13:47:58 +0300

    finished header

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import checkoutLogo from "./shopping-cart-solid.svg"
 function Checkout(props) {
     return (
-        <div>
-
+        <div className="checkout">
+          <img src={checkoutLogo}/>
+          <span className="counter">0</span>
         </div>
     );
 }
